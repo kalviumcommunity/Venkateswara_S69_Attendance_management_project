29bbc3b1a8bffd7948b6e664c684b2dbfcdc128f
venkyroy22 <venkateswarakonetisetty@gmail.com>
2025-09-08 09:16:46 +0530

    Done

--- src/com/school/Student.java
+++ src/com/school/Student.java
@@ -3,10 +3,11 @@ package com.school;
 public class Student {
     int studentId;
     String name;
+    private static int nextStudentIdCounter = 1;
 
-    public void setDetails(int id, String name){
-        this.studentId = id;
+    public Student(String name) {
         this.name = name;
+        this.studentId = nextStudentIdCounter++;
     }
 
     public void display(){
