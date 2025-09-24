3b210423505f2ec182d0b206a28ba9f41ebacc72
venkyroy22 <venkateswarakonetisetty@gmail.com>
2025-09-24 09:26:43 +0530

    Done

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -5,14 +5,20 @@ import java.util.List;
 public class Main{
     public static void main(String[] args){
         Student[] student = new Student[2];
-        student[0] = new Student("Tom");
-        student[1] = new Student("Karl");
+        student[0] = new Student("Tom","A");
+        student[1] = new Student("Karl","A");
         Course[] course = new Course[2];
         course[0] = new Course("abc");
         course[1] = new Course("abd");
+        Teacher[] teachers = new Teacher[2];
+        teachers[0] = new Teacher("Tom","English");
+        teachers[1] = new Teacher("Karl","Computer Science");
+        Staff[] staffs = new Staff[2];
+        staffs[0] = new Staff("Tom","Main");
+        staffs[1] = new Staff("Karl","Sub");
         ArrayList<AttendanceRecord> attendanceLog = new ArrayList<AttendanceRecord>();
-        attendanceLog.add(new AttendanceRecord(student[0].getStudentId(), course[0].getCourseId(), "Present"));
-        attendanceLog.add(new AttendanceRecord(student[1].getStudentId(),course[1].getCourseId() ,"null"));
+        attendanceLog.add(new AttendanceRecord(student[0].getId(), course[0].getCourseId(), "Present"));
+        attendanceLog.add(new AttendanceRecord(student[1].getId(),course[1].getCourseId() ,"null"));
 
         for(AttendanceRecord attend : attendanceLog){
             attend.displayRecord();
@@ -20,6 +26,14 @@ public class Main{
 
         for(Course c: course){
             c.display();
+        }
+
+        for(Teacher t: teachers){
+            t.displayDetails();
+        }
+
+        for(Staff s: staffs){
+            s.displayDetails();
         }
         System.out.println("Welcome");
 }       
